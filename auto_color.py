28fc86cf18fcdf08e9569973a46b374df0624b0d
Hugo Posca <hposca@users.noreply.github.com>
2017-02-13 18:05:08 -0200

    Improve colorization script
    Now instead of fixed strings we can use the --nodes flag to pass the
    nodes that we wish to start colorizing from.
    Also instead of a dotfile with fixed name we should use the --dotfile
    flag to point to the file we want to use.

--- auto_color.py
+++ auto_color.py
@@ -1,50 +1,68 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import subprocess
+import argparse
 import fileinput
+import os
 import re
+import subprocess
 
-dot_file = "dotfile_auto_color"
-new_file = "dotfile_auto_color.new"
-with open(dot_file, 'r') as infile, open('multi_sed.sh', 'w') as outfile:
-    initial_string = "awseb_e_rhivxhxfy4_stack_AWSEBAutoScalingGroup_WZUHOPA8XW3C"
-    color = "darkolivegreen2"
+# Full list of colors in:
+# http://www.graphviz.org/doc/info/colors.html
+colors = ['darkolivegreen2', 'cadetblue2', 'gold2', 'coral1', 'hotpink2', 'royalblue2']
 
-    array = [ initial_string ]
+def colorize(dotfile, nodes):
+    colorizes_dotfile = dotfile + '.colorized'
+    output_script = 'colorize-{}.sh'.format(dotfile)
 
-    outfile.write("#!/usr/bin/env bash\n\n")
-    outfile.write("cp {} {}\n\n".format(dot_file, new_file))
-    for element in array:
-        if not element.startswith('subnet'):
-            text = 'sed -i "s/\({} -> [a-zA-Z0-9_]*\) ;/\\1 [penwidth=7;color={}] ;/" {}\n'.format(element, color, new_file)
-            outfile.write(text)
+    with open(output_script, 'w') as outfile:
+        outfile.write("#!/usr/bin/env bash\n\n")
+        outfile.write("cp {} {}\n\n".format(dotfile, colorizes_dotfile))
 
-            for line in infile:
-                next_elem_regex = re.compile('{} -> ([a-zA-Z0-9_]*) ;'.format(element))
-                match_result = next_elem_regex.search(line)
-                if match_result:
-                    next_element = match_result.group(1)
-                    if not next_element in array:
-                        array.append(next_element)
+        with open(dotfile, 'r') as infile:
+            for node in nodes:
+                color = colors[nodes.index(node)]
 
-            infile.seek(0,0)
+                elements = [ node ]
 
-    initial_string = "awseb_e_spxwp5tmjz_stack_AWSEBAutoScalingGroup_J10G3OFJY0OA"
-    color = "cadetblue2"
-    array = [ initial_string ]
+                for element in elements:
+                    if not element.startswith('subnet'):
+                        text = 'sed -i "s/\({} -> [a-zA-Z0-9_]*\) ;/\\1 [penwidth=7;color={}] ;/" {}\n'.format(element, color, colorizes_dotfile)
+                        outfile.write(text)
 
-    for element in array:
-        if not element.startswith('subnet'):
-            text = 'sed -i "s/\({} -> [a-zA-Z0-9_]*\) ;/\\1 [penwidth=7;color={}] ;/" {}\n'.format(element, color, new_file)
-            outfile.write(text)
+                        for line in infile:
+                            next_elem_regex = re.compile('{} -> ([a-zA-Z0-9_]*) ;'.format(element))
+                            match_result = next_elem_regex.search(line)
+                            if match_result:
+                                next_element = match_result.group(1)
+                                if not next_element in elements:
+                                    elements.append(next_element)
 
-            for line in infile:
-                next_elem_regex = re.compile('{} -> ([a-zA-Z0-9_]*) ;'.format(element))
-                match_result = next_elem_regex.search(line)
-                if match_result:
-                    next_element = match_result.group(1)
-                    if not next_element in array:
-                        array.append(next_element)
+                        infile.seek(0,0)
+                outfile.write('\n')
+    os.chmod(output_script, 0o755)
 
-            infile.seek(0,0)
+def parse_arguments():
+    description = '''Program created to get a generated dotfile, which represents
+an AWS infrastructure, and colorize paths starting from the given node.'''
+
+    epilog = '''After execution this script will generate a colorize-DOTFILE.sh script that
+will actually modify (copy) the DOTFILE to colorize. And this new dotfile can
+be sent to `dot` to generate a beautiful PNG image.
+
+You can try to oneline everything with something like this:
+./mapall.py --region us-east-1 --security > dotfile && ./auto_color.py --dotfile dotfile --nodes node1 node2 && ./colorize-dotfile.sh && dot -Tpng dotfile.colorized > dotfile.colorized.png '''
+
+    parser = argparse.ArgumentParser(description=description, epilog=epilog, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--dotfile', required=True, help='Dotfile to use')
+    parser.add_argument('--nodes', nargs='+',
+        required=True,
+        help='''The list to use as starting nodes. Must be strings and in the
+format that is in the dotfile, i.e. underscores instead
+of dashes.''')
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    colorize(args.dotfile, args.nodes)
